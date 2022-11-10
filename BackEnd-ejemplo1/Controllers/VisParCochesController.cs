9f83c0a9cfeff13b3dffe635cadbada079db9d26
Armani666 <abiram_sanser@hotmail.com>
2022-11-10 17:24:09 -0600

    Segundo commit

---
+++ BackEnd-ejemplo1/Controllers/VisParCochesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BackEnd_ejemplo1.Models;
+
+namespace BackEnd_ejemplo1.Controllers
+{
+    public class VisParCochesController : Controller
+    {
+        // GET: VisParCoches
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult _Garage()
+        {
+            return View("_Garage", new Garage());
+        }
+        public ActionResult _GarageSimple()
+        {
+            return View("_GarageSimple", new Garage());
+        }
+       
+    }
+}
---
+++ BackEnd-ejemplo1/Models/Garage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BackEnd_ejemplo1.Models
+{
+    public class Garage:List<Coches>
+    {
+       public Garage()
+        {
+
+            this.Add(new Coches("1","Tesla","Roadster","Elon Musk"));
+            this.Add(new Coches("2", "Toyota", "Yaris", "Domingo Garcia"));
+            this.Add(new Coches("3", "Honda", "Civic", "Antonio León"));
+            this.Add(new Coches("4", "Ferrari", "F40", "Roberto Cardiel"));
+
+        }
+
+    }
+}
